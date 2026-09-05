5767a1f9405871f918a12da599f3cd4480ce3cc4
Alex Muha <dev.alex.muha@gmail.com>
2026-09-05 21:13:35 +0300

    Initiall commit

---
+++ database/migrations/2026_09_05_152007_create_user_profiles_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_profiles', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->foreignIdFor(User::class)->unique()->constrained()->cascadeOnDelete();
+            $table->string('user_name');
+            $table->string('avatar_uri')->nullable();
+            $table->text('user_bio')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_profiles');
+    }
+};
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+// build db architecture
+// setup factories
+// watch course to get ideas what to add (action classes, introduce myself into automated testing, alpine js...)
+// composer install vs update.
