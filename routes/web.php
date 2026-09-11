06b1920b6a3de72e73bd3ca2ac44fc52c33cc0a2
Alex Muha <dev.alex.muha@gmail.com>
2026-09-11 14:35:48 +0300

    Before build authentication

--- routes/web.php
+++ routes/web.php
@@ -5,11 +5,17 @@ use App\Http\Controllers\TextPostController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [ImagePostController::class, 'index']);
+Route::get('/sol/create', [ImagePostController::class, 'create']);
+Route::post('/sol', [ImagePostController::class, 'store']);
+
 Route::get('/echoes', [TextPostController::class, 'index']);
+Route::get('/echo/create', [TextPostController::class, 'create']);
 
 
 
-
+// build echoes index
+// build / and echoes show
+// work on auth or user register
 
 
 
